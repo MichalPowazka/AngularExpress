2a1edde2a6bef89398369eb83e492e064591099b
Michal <michal.powazka.workplace@gmail.com>
2023-02-06 20:24:09 +0100

    Finishing products routing

--- routes/products.js
+++ routes/products.js
@@ -60,7 +60,6 @@ router.get('/:prodId', (req, res) => {
             'p.title as name',
             'p.price',
             'p.quantity',
-            'p.description',
             'p.image',
             'p.id',
             'p.images'
@@ -76,5 +75,52 @@ router.get('/:prodId', (req, res) => {
             }
         }).catch(err => res.json(err));
 });
+
+//Get ALL PRODUCTS FROM ONE PARTICULAR CATEGORY
+router.get('/category/:catName', (req ,res) => {
+    let page = (req.query.page !== undefined && req.query.page !== 0) ? req.query.page : 1;
+    const limit = (req.query.limit !== undefined && req.query.limit !== 0) ? req.query.limit : 10;   // set limit of items per page
+    let startValue;
+    let endValue;
+    if (page > 0) {
+        startValue = (page * limit) - limit;     // 0, 10, 20, 30
+        endValue = page * limit;                  // 10, 20, 30, 40
+    } else {
+        startValue = 0;
+        endValue = 10;
+    }
+
+    // fetch the category name from the url
+    const cat_title = req.params.catName;
+
+    database.table('products as p')
+        .join([
+            {
+                table: "categories as c",
+                on: `c.id = p.cat_id WHERE c.title LIKE '%${cat_title}%'`
+            }
+        ])
+        .withFields(['c.title as category',
+            'p.title as name',
+            'p.price',
+            'p.quantity',
+            'p.image',
+            'p.id'
+        ])
+        .slice(startValue, endValue)
+        .sort({id: .1})
+        .getAll()
+        .then(prods => {
+            if (prods.length > 0) {
+                res.status(200).json({
+                    count: prods.length,
+                    products: prods
+                });
+            } else {
+                res.json({message: `No products foub from ${cat_title} category.`});
+            }
+        })
+        .catch(err => console.log(err));
+})
 
 module.exports=router;
